0da8a134d328d31521e8e230d6001e57eb668cdb
TamniVilajet <ermin.kirlic@gmail.com>
2022-04-05 18:06:57 +0200

    Exercise 06

---
+++ ex07/src/index.js
@@ -0,0 +1,8 @@
+// Only change code below this line
+function randomRangeNumber(minNumber, maxNumber) {
+
+    Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
+    
+}
+// Only change code above this line
+module.exports = randomRangeNumber;
